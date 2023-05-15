b43e762cdb1e2e0c4eacebcfceb638fa5d6dcd5c
lucas <lucas@DESKTOP-5TLC2TC>
2023-05-15 20:03:02 -0300

    Agregar archivos de proyecto.

---
+++ Clase 7 - Actividad 1 - Cuenta Bancaria/Miembros.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_7___Actividad_1___Cuenta_Bancaria
+{
+    internal class Miembros
+    {
+
+        private long NumeroCuenta;
+        private string Apellido;
+        private string Nombre;
+        private string Direccion;
+        private string FechaNac;
+        private double Saldo;
+
+        public Miembros()
+        {
+        }
+
+        public Miembros(long numeroCuenta, string apellido, string nombre, string direccion, string fechaNac, double saldo)
+        {
+            this.NumeroCuenta = numeroCuenta;
+            this.Apellido = apellido;
+            this.Nombre = nombre;
+            this.Direccion = direccion;
+            this.FechaNac = fechaNac;
+            this.Saldo = saldo;
+        }
+
+        public void SetApellido(string Apellido)
+        {
+            this.Apellido = Apellido;
+        }
+
+        public String GetApellido()
+        {
+            return Apellido;
+        }
+
+        public void SetNombre(string Nombre)
+        {
+            this.Nombre = Nombre;
+        }
+
+        public String GetNombre()
+        {
+            return Nombre;
+        }
+
+        public void SetDireccion(string Direccion)
+        {
+            this.Direccion = Direccion;
+        }
+
+        public String GetDireccion()
+        {
+            return Direccion;
+        }
+
+        public void SetFechaNac(string FechaNac)
+        {
+            this.FechaNac = FechaNac;
+        }
+
+        public String GetFechaNac()
+        {
+            return FechaNac;
+        }
+
+        public double GetSaldo()
+        {
+            return Saldo;
+        }
+     
+        public double DepositarDinero(double amount) 
+        {
+            Saldo += amount;
+            return Saldo;
+        }
+
+        public double RetirarDinero(double amount)
+        {
+            if(amount < Saldo)
+            {
+                Saldo -= amount;
+                return Saldo;
+            }
+            else
+            {
+                return -1; //saldo insuficiente
+            }
+        }
+
+    }
+}
---
+++ Clase 7 - Actividad 1 - Cuenta Bancaria/Program.cs
@@ -0,0 +1,33 @@
+﻿using Clase_7___Actividad_1___Cuenta_Bancaria;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace actividad12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Miembros miembros = new Miembros(123456789, "González", "Juan", "Calle Principal 123", "01/01/1990", 1000.0);
+
+            Console.WriteLine("Apellido: " + miembros.GetApellido());
+            Console.WriteLine("Nombre: " + miembros.GetNombre());
+            Console.WriteLine("Dirección: " + miembros.GetDireccion());
+            Console.WriteLine("Fecha de Nacimiento: " + miembros.GetFechaNac());
+            Console.WriteLine("Saldo inicial: " + miembros.GetSaldo());
+
+            double saldoActualizado = miembros.DepositarDinero(500.0);
+            Console.WriteLine("Saldo después del depósito: " + saldoActualizado);
+
+            saldoActualizado = miembros.RetirarDinero(200.0);
+            if (saldoActualizado != -1)
+            {
+                Console.WriteLine("Saldo después del retiro: " + saldoActualizado);
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para realizar el retiro.");
+            }
+        }
+    }
+}
